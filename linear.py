15eb9b2689b6413d7d428256caab1306a0c0ebea
vdoffan <vdoffan@yandex.ru>
2024-11-10 17:31:54 +0300

    changed programms and added trained model

--- linear.py
+++ linear.py
@@ -1,47 +1,82 @@
 import torch
 from torchvision import datasets, transforms
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
 import secrets
+from linearTraining import LinearNN, evaluate
+from sklearn.metrics import accuracy_score
 
 
-class LinearNN(nn.Module):
-    def __init__(self):
-        super(LinearNN, self).__init__()
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(784, 500)
-        self.fc2 = nn.Linear(500, 200)
-        self.fc3 = nn.Linear(200, 64)
-    
-    def forward(self, x):
-        x = self.flatten(x)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+strl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-model = LinearNN()
-model.load_state_dict(torch.load('./linear_model.pth'))
 
-test_data = datasets.EMNIST('./emnist-data', split='letters', train=False, download=True, transform=transforms.ToTensor())
-
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=128, shuffle=False)
-
-strl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-fig, axes = plt.subplots(2, 5, figsize=(10, 4))
 def getChank(data):
+    chanks = []
+    fig, axes = plt.subplots(2, 5, figsize=(10, 4))
     for i in range(10):
         random_index = secrets.randbelow(len(data))
         test_sample, sample_class = data[random_index]
+        chanks.append((test_sample, sample_class))
         image = test_sample.squeeze(0)
         ax = axes[i // 5, i % 5]
         ax.imshow(image)
-        ax.set_title(f"Label: {strl[sample_class-1]}, ({sample_class})")
-        ax.axis('off')
+        ax.set_title(f"Label: {strl[sample_class-1]}")
+        ax.axis("off")
 
+    fig.suptitle("Картинки букв для распознавания")
     plt.tight_layout()
     plt.show()
 
+    return chanks
+
+
+def evaluateOnPics(model, data):
+    test_chanks = getChank(data)
+    y_pred_list = []
+    y_true_list = []
+    for i, chank in enumerate(test_chanks):
+        x_test, y_true = chank
+        logits = model(x_test.to(device))
+
+        y_pred = torch.argmax(logits, dim=1)
+
+        print(f"Для буквы {strl[y_true - 1]} предсказание {strl[y_pred - 1]}")
+
+        y_pred_list.append(y_pred)
+        y_true_list.append(y_true)
+
+    accuracy = accuracy_score(y_true_list, y_pred_list)
+    return accuracy
+
+
+def main():
+    print("В процессе работы программы появится картинка с буквами для предсказания.")
+    print("Чтоы продолжить работу программы, закройте эту картинку.")
+
+    model = LinearNN()
+    model.load_state_dict(torch.load("linear_model.pth", weights_only=True))
+    model.eval()
+
+    test_data = datasets.EMNIST(
+        "./emnist-data",
+        split="letters",
+        train=False,
+        download=True,
+        transform=transforms.ToTensor(),
+    )
+
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=128, shuffle=True)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    test_acc, test_loss = evaluate(model, test_loader, loss_fn)
+    print(
+        "Для распознавания букв используется нейросеть со следующими характеристиками:"
+    )
+    print("Точность на тестовой выборке:", test_acc, "Loss на тестовой выборке:", test_loss)
+
+    accuracy = evaluateOnPics(model, test_data)
+    print(f"Точность предсказаний для данных букв: {int(accuracy*10)}/10")
+
+
+if __name__ == "__main__":
+    main()
--- linearTraining.py
+++ linearTraining.py
@@ -6,6 +6,9 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
 class LinearNN(nn.Module):
     def __init__(self):
         super(LinearNN, self).__init__()
@@ -13,7 +16,7 @@ class LinearNN(nn.Module):
         self.fc1 = nn.Linear(784, 500)
         self.fc2 = nn.Linear(500, 200)
         self.fc3 = nn.Linear(200, 64)
-    
+
     def forward(self, x):
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
@@ -22,40 +25,19 @@ class LinearNN(nn.Module):
 
         return x
 
-train_data = datasets.EMNIST(root='./emnist-data', split='letters', train=True, download=True, transform=transforms.ToTensor())
-test_data = datasets.EMNIST(root='./emnist-data', split='letters', train=False, download=True, transform=transforms.ToTensor())
-
-train_size = int(len(train_data)*0.8)
-val_size = len(train_data) - train_size
-train_data, val_data = torch.utils.data.random_split(train_data, [train_size, val_size])
-
-
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_data, batch_size=128, shuffle=False)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=128, shuffle=False)
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-model = LinearNN().to(device)
-
-learning_rate = 1e-3
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
-
 
 def evaluate(model, dataloader, loss_fn):
     y_pred_list = []
     y_true_list = []
     losses = []
-    
+
     for i, batch in enumerate(dataloader):
         x_batch, y_batch = batch
-        
+
         with torch.no_grad():
             logits = model(x_batch.to(device))
             loss = loss_fn(logits, y_batch.to(device))
-            
+
             loss = loss.item()
             losses.append(loss)
 
@@ -63,40 +45,80 @@ def evaluate(model, dataloader, loss_fn):
 
         y_pred_list.extend(y_pred.cpu().numpy())
         y_true_list.extend(y_batch.numpy())
-    
+
     accuracy = accuracy_score(y_true_list, y_pred_list)
-    
+
     return accuracy, np.mean(losses)
 
 
-def train(model, loss_fn, optimizer, n_epochs=10):
+def train(model, loss_fn, optimizer, dataloaders, n_epochs=10):
     model.train(True)
-    
+
+    train_loader, val_loader = dataloaders
+
     for epoch in range(n_epochs):
-        print(f'On epoch: {epoch+1}/{n_epochs}')
+        print(f"On epoch: {epoch+1}/{n_epochs}")
 
         for i, batch in enumerate(train_loader):
             x_batch, y_batch = batch
-            
+
             logits = model(x_batch.to(device))
-            
+
             loss = loss_fn(logits, y_batch.to(device))
-            
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
         train_acc, train_loss = evaluate(model, train_loader, loss_fn)
-        print('Train acc:', train_acc, 'Train loss:', train_loss)
+        print("Train acc:", train_acc, "Train loss:", train_loss)
 
         val_acc, val_loss = evaluate(model, val_loader, loss_fn)
-        print('Val acc:', val_acc, 'Val loss:', val_loss)
+        print("Val acc:", val_acc, "Val loss:", val_loss)
 
     return model
 
-model = train(model, loss_fn, optimizer, 6)
 
-test_acc, test_loss = evaluate(model, test_loader, loss_fn)
-print('Test acc:', test_acc, 'Test loss:', test_loss)
+def main():
+    train_data = datasets.EMNIST(
+        root="./emnist-data",
+        split="letters",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor(),
+    )
 
-torch.save(model.state_dict(), 'linear_model.pth')
+    test_data = datasets.EMNIST(
+        root="./emnist-data",
+        split="letters",
+        train=False,
+        download=True,
+        transform=transforms.ToTensor(),
+    )
+
+    train_size = int(len(train_data) * 0.8)
+    val_size = len(train_data) - train_size
+    train_data, val_data = torch.utils.data.random_split(
+        train_data, [train_size, val_size]
+    )
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=128, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=128, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=128, shuffle=False)
+
+    model = LinearNN().to(device)
+
+    learning_rate = 5e-4
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+
+    model = train(model, loss_fn, optimizer, (train_loader, val_loader), 6)
+
+    test_acc, test_loss = evaluate(model, test_loader, loss_fn)
+    print("Test acc:", test_acc, "Test loss:", test_loss)
+
+    torch.save(model.state_dict(), "linear_model.pth")
+
+
+if __name__ == "__main__":
+    main()
