2b0c340d1a536add10884220fdf2bad60f5bd0df
vdoffan <vdoffan@yandex.ru>
2024-11-20 17:43:13 +0300

    added some changes

--- convTraining.py
+++ convTraining.py
@@ -5,99 +5,135 @@ import torch.nn.functional as F
 import numpy as np
 from sklearn.metrics import accuracy_score
 
+# Устройство для вычислений (GPU, если доступен)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+test_transform = transforms.Compose(
+    [
+        transforms.Grayscale(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ]
+)
 
-# Улучшенная модель
+# Загрузка данных с указанием пути к папке и трансформаций
+test_data = datasets.ImageFolder(root="datasets/test", transform=test_transform)
+
+# Создание DataLoader для тренировки и тестирования
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
+
+
+# Определение нейронной сети с несколькими сверточными слоями
 class ConvNN(nn.Module):
     def __init__(self):
         super(ConvNN, self).__init__()
+        # Сверточные слои с 32, 64 и 128 фильтрами соответственно
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        # self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        # Слой максимального пулинга
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, 26)
-        self.dropout = nn.Dropout(0.5)
+        # Полносвязные слои
+        self.fc1 = nn.Linear(64 * 8 * 8, 700)
+        self.fc2 = nn.Linear(700, 26)
+        # Слой Dropout для регуляризации
+        self.dropout = nn.Dropout(0.6)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))  # [batch, 32, 16, 16]
-        x = self.pool(F.relu(self.conv2(x)))  # [batch, 64, 8, 8]
-        x = self.pool(F.relu(self.conv3(x)))  # [batch, 128, 4, 4]
-        x = x.view(-1, 128 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
+        # Применение сверток и пулинга с активацией ReLU
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        # x = self.pool(F.relu(self.conv3(x)))
+        # Изменение формы тензора перед подачей в полносвязный слой
+        x = x.view(-1, 64 * 8 * 8)
+        x = F.relu(self.fc1(x))  # Активация ReLU для первого полносвязного слоя
+        x = self.dropout(x)  # Применение Dropout
+        x = self.fc2(x)  # Выходной слой
         return x
 
 
+# Функция для оценки точности модели на данных (train или test)
 def evaluate(model, dataloader, loss_fn, device):
+    model.eval()
     y_pred_list = []
     y_true_list = []
     losses = []
 
-    for i, batch in enumerate(dataloader):
+    # Итерация по батчам данных
+    for batch in dataloader:
         X_batch, y_batch = batch
 
-        with torch.no_grad():
-            logits = model(X_batch.to(device))
+        with torch.no_grad():  # Выключаем градиенты для оценки
+            logits = model(X_batch.to(device))  # Получаем прогнозы модели
 
-            loss = loss_fn(logits, y_batch.to(device))
-
-            loss = loss.item()
+            loss = loss_fn(logits, y_batch.to(device))  # Вычисление потерь
+            loss = loss.item()  # Получение значения потерь
             losses.append(loss)
 
-            y_pred = torch.argmax(logits, dim=1)
+            y_pred = torch.argmax(
+                logits, dim=1
+            )  # Выбор класса с максимальной вероятностью
 
+        # Добавление предсказаний и истинных значений для расчета точности
         y_pred_list.extend(y_pred.cpu().numpy())
         y_true_list.extend(y_batch.numpy())
 
+    # Вычисление точности и средней потери
     accuracy = accuracy_score(y_true_list, y_pred_list)
-
     return accuracy, np.mean(losses)
 
 
+# Функция тренировки модели
 def train(model, loss_fn, optimizer, dataloader, n_epochs=6):
-    model.train(True)
-
     train_loader = dataloader
-    for epoch in range(n_epochs):
-        print(f"On epoch {epoch+1}/{n_epochs}")
-
-        for i, batch in enumerate(train_loader):
+    for epoch in range(n_epochs+1):
+        model.train(True)  # Устанавливаем модель в режим тренировки
+        for batch in train_loader:
             X_batch, y_batch = batch
 
-            logits = model(X_batch.to(device))
-            loss = loss_fn(logits, y_batch.to(device))
+            logits = model(X_batch.to(device))  # Получение прогноза модели
+            loss = loss_fn(logits, y_batch.to(device))  # Вычисление потерь
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            loss.backward()  # Вычисление градиентов
+            optimizer.step()  # Обновление параметров модели
+            optimizer.zero_grad()  # Обнуляем градиенты
 
-        train_acc, train_loss = evaluate(model, train_loader, loss_fn, device)
-        print(f"Train accuracy: {train_acc}. Train loss: {train_loss}")
+        if epoch % 25 == 0:
+            print(f"On epoch {epoch}/{n_epochs}")
+
+            # Оценка точности и потерь на тренировочных данных
+            train_acc, train_loss = evaluate(model, train_loader, loss_fn, device)
+            print(f"Train accuracy: {train_acc}. Train loss: {train_loss}")
+            test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)
+            print(f"Test accuracy: {test_acc}. Test loss: {test_loss}")
+
 
     return model
 
 
+# Основная функция
 def main():
-    model = ConvNN()
-    model.to(device)
+    model = ConvNN()  # Создаем модель
+    model.to(device)  # Перемещаем модель на выбранное устройство (GPU или CPU)
 
+    # Определение трансформаций для тренировочных данных
     train_transform = transforms.Compose(
         [
-            transforms.Grayscale(),
-            transforms.RandomRotation(10),
-            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),
-            transforms.RandomHorizontalFlip(),
+            transforms.Grayscale(),  # Преобразование в оттенки серого
+            transforms.RandomRotation(10),  # Случайное вращение
+            transforms.RandomAffine(
+                degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)
+            ),  # Аффинные преобразования
+            transforms.RandomHorizontalFlip(),  # Случайное горизонтальное отражение
             transforms.ColorJitter(
                 brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2
-            ),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,)),
+            ),  # Случайные изменения яркости, контраста, насыщенности и оттенка
+            transforms.ToTensor(),  # Преобразование в тензор
+            transforms.Normalize((0.5,), (0.5,)),  # Нормализация
         ]
     )
 
+    # Трансформация для тестовых данных (без аугментаций)
     test_transform = transforms.Compose(
         [
             transforms.Grayscale(),
@@ -106,26 +142,29 @@ def main():
         ]
     )
 
+    # Загрузка данных с указанием пути к папке и трансформаций
     train_data = datasets.ImageFolder(root="datasets/train", transform=train_transform)
-
     test_data = datasets.ImageFolder(root="datasets/test", transform=test_transform)
 
-    print(train_data.classes)
+    # Создание DataLoader для тренировки и тестирования
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
-
+    # Определение гиперпараметров и оптимизатора
     learning_rate = 1e-3
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    loss_fn = nn.CrossEntropyLoss()
+    loss_fn = nn.CrossEntropyLoss()  # Кросс-энтропия для задачи классификации
 
-    model = train(model, loss_fn, optimizer, train_loader, n_epochs=400)
+    # Тренировка модели
+    model = train(model, loss_fn, optimizer, train_loader, n_epochs=500)
 
+    # Оценка точности на тестовых данных
     test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)
     print(f"Test accuracy: {test_acc}. Test loss: {test_loss}")
 
+    # Сохранение обученной модели
     torch.save(model.state_dict(), "conv_model.pth")
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Запуск основной функции
--- main.py
+++ main.py
@@ -5,82 +5,114 @@ import secrets
 from convTraining import ConvNN, evaluate
 from sklearn.metrics import accuracy_score
 
+# Определение устройства для вычислений (GPU, если доступен)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Строка с алфавитом для отображения меток классов
 strl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
+# Выбирает случайные 10 изображений из набора данных и отображает их
 def getChank(data):
     chanks = []
+    # Создание сетки для отображения изображений (2 строки по 5 столбцов)
     fig, axes = plt.subplots(2, 5, figsize=(10, 4))
+
     for i in range(10):
+        # Выбор случайного индекса
         random_index = secrets.randbelow(len(data))
+        # Получение изображения и его класса
         test_sample, sample_class = data[random_index]
         chanks.append((test_sample, sample_class))
+        # Удаление канала цвета для отображения в оттенках серого
         image = test_sample.squeeze(0)
+        # Выбор соответствующей оси для отображения изображения
         ax = axes[i // 5, i % 5]
-        ax.imshow(image, cmap='gray')  # Убедитесь, что изображение отображается в градациях серого
+        ax.imshow(image, cmap="gray")
         ax.set_title(f"Label: {strl[sample_class]}")
         ax.axis("off")
 
+    # Установка заголовка для всей фигуры
     fig.suptitle("Картинки букв для распознавания")
     plt.tight_layout()
     plt.show()
 
     return chanks
 
+
+# Оценивает модель на случайных изображениях и выводит предсказания
 def evaluateOnPics(model, data):
+    # Получение случайных изображений для оценки
     test_chanks = getChank(data)
     y_pred_list = []
     y_true_list = []
-    for i, chank in enumerate(test_chanks):
-        x_test, y_true = chank
-        logits = model(x_test.to(device))
 
+    for chank in test_chanks:
+        x_test, y_true = chank
+        # Прогноз модели
+        logits = model(x_test.to(device))
+        # Выбор класса с максимальной вероятностью
         y_pred = torch.argmax(logits, dim=1)
 
-        print(f"Для буквы {strl[y_true - 1]} предсказание {strl[y_pred - 1]}")
+        # Вывод предсказания для текущего изображения
+        print(f"Для буквы {strl[y_true]} предсказание {strl[y_pred]}")
 
-        # Переводим тензоры на CPU и получаем их значения
-        y_pred_list.append(y_pred.cpu().item())  # если y_pred скаляр
-        y_true_list.append(y_true)  # y_true уже является обычным int
+        # Добавление предсказанных и истинных значений в списки
+        y_pred_list.append(y_pred.cpu().item())
+        y_true_list.append(y_true)
 
+    # Вычисление точности
     accuracy = accuracy_score(y_true_list, y_pred_list)
     return accuracy
 
 
+# Основная функция
 def main():
     print("В процессе работы программы появится картинка с буквами для предсказания.")
     print("Чтобы продолжить работу программы, закройте эту картинку.")
 
+    # Создание экземпляра модели
     model = ConvNN()
 
-    # Загрузка модели с указанием map_location
-    model.load_state_dict(torch.load("conv_model.pth", map_location=device))
-    model.to(device)  # Перемещаем модель на устройство
-    model.eval()
+    # Загрузка сохраненных весов модели
+    model.load_state_dict(
+        torch.load("conv_model.pth", weights_only=True, map_location=device)
+    )
+    model.to(device)  # Перемещение модели на устройство
+    model.eval()  # Установка модели в режим оценки
 
-    # Преобразования для тестового набора
-    test_transform = transforms.Compose([
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
+    # Определение трансформаций для тестовых данных
+    test_transform = transforms.Compose(
+        [
+            transforms.Grayscale(),  # Преобразование в оттенки серого
+            transforms.ToTensor(),  # Преобразование в тензор
+            transforms.Normalize((0.5,), (0.5,)),  # Нормализация
+        ]
+    )
 
-    # Загружаем тестовый набор данных
-    test_data = datasets.ImageFolder(root='datasets/test', transform=test_transform)
-
+    # Загрузка тестовых данных
+    test_data = datasets.ImageFolder(root="datasets/test", transform=test_transform)
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
 
+    # Определение функции потерь
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)  # Передаём device в evaluate
-
+    # Оценка модели на тестовых данных
+    test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)
     print(
         "Для распознавания букв используется нейросеть со следующими характеристиками:"
     )
-    print("Точность на тестовой выборке:", test_acc, "Loss на тестовой выборке:", test_loss)
+    print(
+        "Точность на тестовой выборке:",
+        test_acc,
+        "Loss на тестовой выборке:",
+        test_loss,
+    )
 
+    # Оценка модели на случайных изображениях
     accuracy = evaluateOnPics(model, test_data)
     print(f"Точность предсказаний для данных букв: {int(accuracy*10)}/10")
 
+
 if __name__ == "__main__":
-    main()
+    main()  # Запуск основной функции
