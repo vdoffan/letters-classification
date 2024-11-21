0762c8c21412efbb06ebae03d0fadac72e39743e
vdoffan <vdoffan@yandex.ru>
2024-11-21 13:43:32 +0300

    added new dataset and training function

--- convTraining.py
+++ convTraining.py
@@ -8,20 +8,6 @@ from sklearn.metrics import accuracy_score
 # Устройство для вычислений (GPU, если доступен)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-test_transform = transforms.Compose(
-    [
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)),
-    ]
-)
-
-# Загрузка данных с указанием пути к папке и трансформаций
-test_data = datasets.ImageFolder(root="datasets/test", transform=test_transform)
-
-# Создание DataLoader для тренировки и тестирования
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
-
 
 # Определение нейронной сети с несколькими сверточными слоями
 class ConvNN(nn.Module):
@@ -30,20 +16,18 @@ class ConvNN(nn.Module):
         # Сверточные слои с 32, 64 и 128 фильтрами соответственно
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        # self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         # Слой максимального пулинга
         self.pool = nn.MaxPool2d(2, 2)
         # Полносвязные слои
-        self.fc1 = nn.Linear(64 * 8 * 8, 700)
-        self.fc2 = nn.Linear(700, 26)
+        self.fc1 = nn.Linear(64 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 26)
         # Слой Dropout для регуляризации
-        self.dropout = nn.Dropout(0.6)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
         # Применение сверток и пулинга с активацией ReLU
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        # x = self.pool(F.relu(self.conv3(x)))
         # Изменение формы тензора перед подачей в полносвязный слой
         x = x.view(-1, 64 * 8 * 8)
         x = F.relu(self.fc1(x))  # Активация ReLU для первого полносвязного слоя
@@ -84,10 +68,11 @@ def evaluate(model, dataloader, loss_fn, device):
 
 
 # Функция тренировки модели
-def train(model, loss_fn, optimizer, dataloader, n_epochs=6):
+def train(model, loss_fn, optimizer, dataloader, n_epochs=100):
+    model.train(True)  # Устанавливаем модель в режим тренировки
     train_loader = dataloader
-    for epoch in range(n_epochs+1):
-        model.train(True)  # Устанавливаем модель в режим тренировки
+
+    for epoch in range(n_epochs + 1):
         for batch in train_loader:
             X_batch, y_batch = batch
 
@@ -104,9 +89,6 @@ def train(model, loss_fn, optimizer, dataloader, n_epochs=6):
             # Оценка точности и потерь на тренировочных данных
             train_acc, train_loss = evaluate(model, train_loader, loss_fn, device)
             print(f"Train accuracy: {train_acc}. Train loss: {train_loss}")
-            test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)
-            print(f"Test accuracy: {test_acc}. Test loss: {test_loss}")
-
 
     return model
 
@@ -120,7 +102,7 @@ def main():
     train_transform = transforms.Compose(
         [
             transforms.Grayscale(),  # Преобразование в оттенки серого
-            transforms.RandomRotation(10),  # Случайное вращение
+            transforms.RandomRotation(15),  # Случайное вращение
             transforms.RandomAffine(
                 degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)
             ),  # Аффинные преобразования
@@ -147,16 +129,16 @@ def main():
     test_data = datasets.ImageFolder(root="datasets/test", transform=test_transform)
 
     # Создание DataLoader для тренировки и тестирования
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
 
     # Определение гиперпараметров и оптимизатора
-    learning_rate = 1e-3
+    learning_rate = 5e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.CrossEntropyLoss()  # Кросс-энтропия для задачи классификации
 
     # Тренировка модели
-    model = train(model, loss_fn, optimizer, train_loader, n_epochs=500)
+    model = train(model, loss_fn, optimizer, train_loader, n_epochs=200)
 
     # Оценка точности на тестовых данных
     test_acc, test_loss = evaluate(model, test_loader, loss_fn, device)
